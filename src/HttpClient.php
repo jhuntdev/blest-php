5e497947669db7bd6ff4a9ef3253b304185b18e7
Admin <admin@Admins-MacBook-Pro.local>
2023-06-20 14:19:45 -0400

    0.0.1

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -1,6 +1,57 @@
 <?php
 namespace BLEST\BLEST;
 
+if (!function_exists('is_list')) {
+  function is_list($value) {
+    if (!is_array($value)) {
+      return false;
+    }
+    $count = count($value);
+    for ($i = 0; $i < $count; $i++) {
+      if (!array_key_exists($i, $value)) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+class EventEmitter {
+  private $listeners = [];
+
+  public function on($event, $callback) {
+    if (!isset($this->listeners[$event])) {
+      $this->listeners[$event] = [];
+    }
+    $this->listeners[$event][] = $callback;
+  }
+
+  public function emit($event, ...$args) {
+    if (isset($this->listeners[$event])) {
+      foreach ($this->listeners[$event] as $listener) {
+        call_user_func_array($listener, $args);
+      }
+    }
+  }
+
+  public function once($event, $callback) {
+    $onceCallback = function (...$args) use ($event, $callback) {
+      $this->removeListener($event, $onceCallback);
+      call_user_func_array($callback, $args);
+    };
+    $this->on($event, $onceCallback);
+  }
+
+  public function removeListener($event, $callback) {
+    if (isset($this->listeners[$event])) {
+      $index = array_search($callback, $this->listeners[$event], true);
+      if ($index !== false) {
+        unset($this->listeners[$event][$index]);
+      }
+    }
+  }
+}
+
 class HttpClient {
   private $url;
   private $options;
--- src/HttpServer.php
+++ src/HttpServer.php
@@ -1,6 +1,21 @@
 <?php
 namespace BLEST\BLEST;
 
+if (!function_exists('is_list')) {
+  function is_list($value) {
+    if (!is_array($value)) {
+      return false;
+    }
+    $count = count($value);
+    for ($i = 0; $i < $count; $i++) {
+      if (!array_key_exists($i, $value)) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
 class HttpServer {
 
   private $request_handler;
